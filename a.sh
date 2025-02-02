a7368d81de5c930001e256ba8e00b9687dcb36ee
amoedo7 <juan.amoedo7@gmail.com>
2025-02-01 23:57:28 -0300

    Actualización de archivos de la página rápido

--- a.sh
+++ a.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
 
-# Asegurarse de estar en la rama correcta                         echo "Verificando rama actual..."
+# Asegurarse de estar en la rama correcta
+echo "Verificando rama actual..."
 current_branch=$(git rev-parse --abbrev-ref HEAD)
-if [[ "$current_branch" != "main" ]]; then                            echo "Estás en la rama '$current_branch'. Cambiando a 'main'."
+if [[ "$current_branch" != "main" ]]; then
+    echo "Estás en la rama '$current_branch'. Cambiando a 'main'."
     git checkout main
-fi                                                                
+fi
+
 # Verificar si hay cambios antes de proceder
 echo "Verificando cambios..."
 git status -s
 if [[ $(git status -s) == "" ]]; then
-    echo "No hay cambios para subir. Saliendo del script."            exit 0
+    echo "No hay cambios para subir. Saliendo del script."
+    exit 0
 fi
 
 # Agregar todos los archivos al staging area
 echo "Agregando archivos..."
-git add .                                                         
-# Crear el commit con un mensaje automático                       commit_message="Actualización de archivos de la página rápido"
+git add .
+
+# Crear el commit con un mensaje automático
+commit_message="Actualización de archivos de la página rápido"
 echo "Haciendo commit con el mensaje: $commit_message"
 git commit -m "$commit_message"
 
-# Empujar los cambios a la rama 'main' del repositorio remoto
-echo "Empujando cambios a GitHub..."                              git push origin main
+# Empujar los cambios a GitHub con el token
+echo "Empujando cambios a GitHub..."
+git push https://amoedo7:$GITHUB_TOKEN@github.com/amoedo7/viajamo.git main
 
 # Confirmación de éxito
 echo "¡Cambios empujados exitosamente!"
